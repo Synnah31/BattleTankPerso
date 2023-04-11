0e86a87cd5c75e033513a39e45edcd66c392d1c1
Synnah31 <jules.ducuing@gmail.com>
2023-04-11 16:47:52 +0200

    docs - Diagrammes
    Add Diagramme-Archi.png in content browser

--- Source/TankBattle/TankAimingComponent.cpp
+++ Source/TankBattle/TankAimingComponent.cpp
@@ -37,7 +37,7 @@ void UTankAimingComponent::TickComponent(float DeltaTime, ELevelTick TickType, F
 	// ...
 }
 
-void UTankAimingComponent::AimAt(FVector HitLocation, /*float LaunchSpeed*/)
+void UTankAimingComponent::AimAt(FVector HitLocation)
 {
 	if (!Canon) { return; }
 
